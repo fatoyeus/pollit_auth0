f6dd0ff8f0f057f795a518c647eaac49725e03fa
Olakunle Fajorin <fatoyeus@hotmail.com>
2022-09-12 11:17:07 +0100

    fix provider

--- deployment_eks/versions.tf
+++ deployment_eks/versions.tf
@@ -2,14 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.15.0"
+      version = ">= 3.20.0"
     }
 
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
     }
   }
-
-  required_version = "~> 1.2.0"
 }
